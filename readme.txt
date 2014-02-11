e8b3cd865c49fe6e034cd779ac6bfa285c401c79
zonej <jzone1229@gmail.com>
2014-02-11 15:02:02 -0500

    Add groundworkCSS Framework. Begin styling of the elements. New fonts from google fonts API. Need to come up with new ideas for styling to be 'creative'

--- readme.txt
+++ readme.txt
@@ -1,6 +1,18 @@
+Joshua M. Zone - zonej - 660892754
+Lab 2 - Weather Forecase API
+
+This application uses the HTML5 geolocation API to obtain a users location if the user allows it, to pass their coordinates to the forecast.io weather API.
+This weather API returns a JSON object of the current weather forecast which is then parsed and written to the DOM using jquery (.html).
+The Coordinates are also passed to the Google Maps API to do a reverse Geocode to get the city name based on the coordinates since forecast.io does not return the city as part of the JSON object. 
+Skycons and HTML canvas are used to create the animated icons for the weather. 
+The Application is styled with the GroudworkCSS css/js framework. 
+
+REFERENCES:
 Reverse Geocoding done via the google maps API
 	https://developers.google.com/maps/documentation/geocoding/?csw=1
 Weather information is attained from the forecast.io API
 	http://forecast.io/
 Icons were attained from:
 	http://darkskyapp.github.io/skycons/
+http://groundworkcss.github.io/ 
+	Framework used for the responsive grid 
--- resources/js/geo.js
+++ resources/js/geo.js
@@ -1,54 +1,85 @@
-$(document).ready(getWeather());
+$(document).ready(function() { 
+	var lat = '42.6957503';
+	var lon = '-73.8966634';
+	
+	/**
+	 * Check if the browser supports Geolocation. 
+	 * If it does then attempt to get the position, if the position is acquired successfully then pass the position to the getCoords function.
+	 * If there is an error getting the position it will be logged to the console and the default (global) values will be passed in to the getWeather function. 
+	 */
+	if(navigator.geolocation) {
+		var timeoutVal = 10 * 1000 * 1000;
+		navigator.geolocation.getCurrentPosition(
+			getCoords,
+			getError,
+			{ enableHighAccuracy: true, 
+				timeout: timeoutVal,
+				maximumAge: 0}
+				);
+	}
+	else {
+		console.log("Geolocation is not supported by this browser");
+		getWeather(lat, lon);
+	}
+	/**
+	 * Check if there was an error when trying to retrieve the current users position from the HTML5 Geolocation API.
+	 * If there is an error then pass the default lat and lon to the getWeather function. 
+	 * @param  Array error list of errors that will be returned. 
+	 * @return error to write to the console for debugging. 
+	 */
+	function getError(error) {
+		var errors = {
+			1: 'Permission denied',
+			2: 'Position unavailable',
+			3: 'Request timeout'
+		};
+		console.log("Error: " + errors[error.code])
+		getWeather(lat, lon);
 
-function getWeather() {
+	}
+});
+
+/**
+ * Attains the lat, lon coords from the HTML5 Geolocation API if the browser supports it. 
+ * @param  position current Position data from the Geolocation API.
+ * @return passes the lat and lon information from the API to the getWeather Function for another API function call. 
+ */
+function getCoords(position) {
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	getWeather(lat, lon);
+}
+
+/**
+ * Function to get all the information from the 2 API calls that are performed. 
+ * JSON Objects are returned and parsed for the correct information which is then displayed in <divs> in the HTML.
+ * @param  lat Latitude passed in either as a default location or from the HTML5 geolocation API data. 
+ * @param  lon Longitude passed in either as a default location or from the HTML5 geolocation API data. 
+ */
+function getWeather(lat, lon) {
 	var apikey = 'bf5483dbc2ddf7ad2be4c320ef75b582';
 	var weatherUrl = 'https://api.forecast.io/forecast/';
 	var mapUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=';
 	var mapEnd = '&sensor=true';
-	var lat = '42.6957503';
-	var lon = '-73.8966634';
 	var data;
 
-	
+	/**
+	 * API Call to do reverse geocoding to get the city name of the current LAT, LON Coordinates
+	 * @param data JSON object of the address based on the current Lat, Lon Coordinates. 
+	 */
 	$.getJSON(mapUrl + lat + "," + lon + mapEnd, function(data) {
 		$('#city').html(data.results[0].address_components[2].short_name);
-		//console.log(data.results[0].address_components[2].short_name);
 	})
+
+	/**
+	 * API Call to get the weather forecast for the current Lat, Lon Coordinates.
+	 * @param data JSON object for the weather forecast of the current coordinates. 
+	 */
 	$.getJSON(weatherUrl + apikey + "/" + lat + "," + lon + "?callback=?", function(data) {
-		//console.log(data.currently.temperature);
-		//console.log(data.currently.summary);
 		var skycons = new Skycons({"color" : "black"});
-		$('#temp').html(data.currently.temperature);
+		$('#temp').html(data.currently.temperature + "&deg");
 		$('#cond').html(data.currently.summary);
-		skycons.set("img", data.currently.icon);
+		skycons.set("skycon-img", data.currently.icon);
 		skycons.play();
-	}); 
+	});
 }
-/*if(navigator.geolocation) {
-	var timeoutVal = 10 * 1000 * 1000;
-	navigator.geolocation.getCurrentPosition(
-		getCoords,
-		getError,
-		{ enableHighAccuracy: true, 
-			timeout: timeoutVal,
-			maximumAge: 0}
-			);
-}
-else {
-	console.log("Geolocation is not supported by this browser");
-}
-
-function getError(error) {
-	var errors = {
-		1: 'Permission denied',
-		2: 'Position unavailable',
-		3: 'Request timeout'
-	};
-	console.log("Error: " + errors[error.code])
-}
-
-function getCoords(position) {
-	var lat = position.coords.latitude;
-	var lon = position.coords.longitude;
-	//console.log("latitude: " + lat + " Longitude: " + lon);
-}*/
